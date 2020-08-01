2e5d54abb9beedffc1477ce177971ff3fbeaedca
mikhailbobrov <mikhailurban13@yandex.ru>
2020-08-01 14:32:09 +0500

    start

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,12 @@ const concat = require('gulp-concat'); //вообще все названия м
 
 const uglify = require('gulp-uglify-es').default;
 
+
+
+
+
+
+
 //сейчас пишем функцию ктр будет подключать browser-sync, но эта функция не является task  - (browsersync пишем с маленькой буквы чтобы не совпадало с константой)
 //browserSync - у каждого пакета gulp есть своя документация -  https://www.browsersync.io/docs  https://www.npmjs.com/package/browser-sync
 function browsersync() {
@@ -29,6 +35,10 @@ function browsersync() {
 
     })
 }
+
+
+
+
 
 
 
@@ -73,13 +83,10 @@ function scripts() {
 
 
 
-//=============================================
-//новая функция которая будет обновлять автоматически страницу
-//при любых изминениях - мы должны вотчить все js файлы - нужно смотреть в документацию - шаблоны Globs
-function startWatch() {
-    watch(['app/**/*.js', '!app/**/*.min.js'], scripts) //так мы выбираем все js файлы в нашем проекте и одновременно ! знак идет после того как мы указали все файлы. Мы исключим все файлы из папки app/**/*.min.js
-    //что происходит - чтобы не было рекурсивного обновления страницы - и рекурсивной сборки - как работает сценарий: мы сохраняем файл app.js - потом инициализируется сборка - все js файлы выгружаются app.min.js - (37.55 минута)
-}
+
+
+
+
 
 
 //установим sass - npm i --save-dev gulp-sass
@@ -104,13 +111,117 @@ function styles() {
         //что делаем дальше -
         .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true})) //grid: true - подкючаем для того,чтобы работало в  ie
 
-        .pipe(cleancss(({ level: {1: {specialComments: 0} }, format: 'beautify' }))) //level - уровень  , format: 'beautify'  - формат вывода
+        .pipe(cleancss(({ level: {1: {specialComments: 0} }, format: 'beautify' }))) //level - уровень  , format: 'beautify'  - формат вывода - Также мы видим закомментированный параметр format: 'beautify'. Если мы его раскомментируем, на выходе мы получим не максимально сжатый CSS код, а наоборот, развёрнутый и читаемый.
 
         //выводим в папку
         .pipe(dest('app/css/'))
+
+        //делаем еще один pipe - в котором гвоорим что нужно делать browsersync - даем команду stream - слежение без перезагрузки
+        .pipe(browserSync.stream())
 }
 
 
+
+
+
+
+
+
+//===============РАБОТА с ИЗОБРАЖЕНИЯМИ - для того чтобы работать с изображениями нам нужен соотвествующий
+// модуль - gulp image min. - установка npm i --save-dev gulp-imagemin - у него настройки по умолчанию дают
+// афигенный результат
+
+//дополнительно нужно следить за теми изображениями которые уже были сжаты - установим дополнительный
+// пакет npm i --save-dev gulp-newer  - вычисляет файлы которые уже были изменены и их уже не трогает
+
+const imagemin = require("gulp-imagemin");
+const newer = require('gulp-newer');
+const del = require('del')
+
+function images() {
+    return src('app/img/src/**/*')
+        .pipe(newer('app/img/dest')) //проверяем тут на переиспользование - если оптимизировали ранее то не будем больше - в ней мы указываем путь куда мы складываем изображения - dest
+        .pipe(imagemin())
+        .pipe(dest('app/img/dest'))
+}
+
+
+//npm i --save-dev gulp-webp
+const webp = require('gulp-webp');
+
+function webP() {
+    return src('app/img/src/**/*.{png,jpg}')
+        .pipe(webp({quality: 90}))
+        .pipe(dest("app/img/dest"));
+}
+
+// npm i --save-dev gulp-svgstore
+// npm i --save-dev gulp-rename
+
+const svgstore = require("gulp-svgstore");
+const rename = require("gulp-rename");
+
+function sprite() {
+    return src("app/img/src/icon-*.svg")
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(rename("sprite.svg"))
+        .pipe(dest("app/img/dest"));
+}
+
+
+//в работе с изображениями иногда необходимо отчистить всю папку dest - ту папку куда мы загружаем изображения - пишем вспомогательный task - cleanimg
+//дополнительно установим пакет del - у самого gulp нет развитой системы для быстрого удаления из файлов системы - npm i --save-dev del
+
+function cleanimg() {
+    return del('app/img/dest/**/*', { force: true})
+}
+
+//добавим функцию очищения папки dist - удалим папку dist - return del
+function cleandist() {
+    return del('dist/**/*', { force: true})
+}
+
+//добавим новую функцию которая будет копировать - сделаем таск который будет собирать все и копировать в отдельную папку
+function buildcopy() {
+    return src([
+        'app/css/**/*.min.css',
+        'app/js/**/*.min.js',
+        'app/img/dest/**/*',
+        'app/**/*.html',
+        ], { base: 'app' })
+        .pipe(dest('dist'));
+
+}
+//base - сделаем с помощью base в dist нормальное адекватное вложение base: 'app' - это путь откуда мы берем все это хозяйство
+//dist - папка куда выгружается готовый проект - это типо build в академии
+
+
+//=============================================
+//новая функция которая будет обновлять автоматически страницу
+//при любых изминениях - мы должны вотчить все js файлы - нужно смотреть в документацию - шаблоны Globs
+function startWatch() {
+    watch(['app/**/*.js', '!app/**/*.min.js'], scripts); //так мы выбираем все js файлы в нашем проекте и одновременно ! знак идет после того как мы указали все файлы. Мы исключим все файлы из папки app/**/*.min.js
+    //что происходит - чтобы не было рекурсивного обновления страницы - и рекурсивной сборки - как работает сценарий: мы сохраняем файл app.js - потом инициализируется сборка - все js файлы выгружаются app.min.js - (37.55 минута)
+    watch('app/**/' + '/**/*', styles);
+    watch('app/**/*.html').on('change', browserSync.reload)
+    watch('app/img/src/**/*', images);
+    watch('app/img/src/**/*', webP);
+    watch('app/img/src/**/*', sprite);
+}
+
+
+
+
+
+
+
+
+
+
+
+//то есть это команды терминала через gulp ... gulp browsersync, gulp scripts.... и тд
 //любые task которые будут использоваться нужно экспортировать - мы можем в экспорте создать
 // комбинацию нужных task
 exports.browsersync = browsersync; //теперь мы сделали экспорт данной функции в task
@@ -121,7 +232,18 @@ exports.scripts = scripts;
 
 exports.styles = styles;
 
+exports.images = images;
 
-//дефолтный task
-exports.default = parallel(scripts, browsersync, startWatch)
+exports.webP = webP;
+
+exports.sprite = sprite;
+
+exports.cleanimg = cleanimg;
+
+exports.build = series(cleandist, styles, scripts, images, buildcopy)
+
+//series - это последовательно
+
+//дефолтный task - Дефолтный таск exports.default позволяет запускать проект одной командой gulp в терминале.
+exports.default = parallel(styles, scripts, browsersync, startWatch)
 
