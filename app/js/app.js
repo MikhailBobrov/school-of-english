683ac74e8ad94f0c2ea0b03a1b4cd41f22b09525
mikhailbobrov <mikhailurban13@yandex.ru>
2020-08-24 18:18:04 +0500

    застрял с меню на 768

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,18 @@
-document.querySelector('.nav-icon3').addEventListener('click', function () {
+const menuButton = document.querySelector('.nav-icon3');
+const menuList = document.querySelector('.site-list');
+const phoneList = document.querySelector('.phone')
 
-    this.classList.toggle('open')
+
+
+menuButton.addEventListener('click', function () {
+    this.classList.toggle('open');
+    let expanded = menuButton.getAttribute('aria-expanded') === 'true' || 'false'
+    this.setAttribute('aria-expanded', !expanded)
+    menuList.classList.toggle('site-list--open')
+    phoneList.classList.toggle('phone__open')
 })
+
+
+
+
+
